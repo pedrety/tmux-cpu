6f4544a5dbd5c660dfc7b3fb2ae8459c70a2a3ff
Pedrety <pedrety998@gmail.com>
2022-03-26 15:47:14 +0100

    Add support for Ryzen 2600 and 2700X cpu temp

--- scripts/cpu_temp.sh
+++ scripts/cpu_temp.sh
@@ -5,8 +5,8 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=scripts/helpers.sh
 source "$CURRENT_DIR/helpers.sh"
 
-cpu_temp_format="%2.0f"
-cpu_temp_unit="C"
+cpu_temp_format="%3.1f"
+cpu_temp_unit="°C"
 
 print_cpu_temp() {
   cpu_temp_format=$(get_tmux_option "@cpu_temp_format" "$cpu_temp_format")
@@ -18,7 +18,7 @@ print_cpu_temp() {
     else
       val="$(sensors)"
     fi
-    echo "$val" | sed -e 's/^Tccd/Core /' | awk -v format="$cpu_temp_format$cpu_temp_unit" '/^Core [0-9]+/ {gsub("[^0-9.]", "", $3); sum+=$3; n+=1} END {printf(format, sum/n)}'
+    echo "$val" | sed -e 's/^Tccd/Core /' - e 's/^Tctl/Core 0/' -e 's/^Tdie/Core 1/' | awk -v format="$cpu_temp_format$cpu_temp_unit" '/^Core [0-9]+/ {gsub("[^0-9.]", "", $3); sum+=$3; n+=1} END {printf(format, sum/n)}'
   fi
 }
 
